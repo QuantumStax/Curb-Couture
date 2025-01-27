f27605b18edeff9ffea00d9a74d33dae16f866e5
Gopisam1126 <gopikrishnan0045@gmail.com>
2025-01-27 22:00:30 +0530

    recover pass

--- FRONTEND/src/assets/pages/forgotPass.jsx
+++ FRONTEND/src/assets/pages/forgotPass.jsx
@@ -1,7 +1,37 @@
+import Footer from "../components/footer";
+import Nav from "../components/nav2";
+
 const ForgorPass = () => {
   return (
-    <div>ForgorPass</div>
-  )
-}
+    <section>
+      <section>
+        <Nav />
+      </section>
+      <section className="flex flex-col items-center bg-primary px-20 py-24">
+        <h1 className="text-4xl font-semibold mb-12">Recover Password</h1>
+        <div>
+          <form>
+            <label htmlFor="reg-mail">Enter your Registered mail id</label>
+            <br />
+            <input
+              type="email"
+              name="reg-mail"
+              id="reg-mail"
+              placeholder="johndoe@gmail.com"
+              className="bg-transparent border border-slate-950 w-[25rem] px-3 py-2 mt-3"
+            />
+          </form>
+          <div className="mt-2">
+            <p>Lost your password? Please enter your email address.</p>
+            <p>You will receive a link to create a new password via email.</p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <Footer/>
+      </section>
+    </section>
+  );
+};
 
-export default ForgorPass
+export default ForgorPass;
--- FRONTEND/src/assets/pages/login.jsx
+++ FRONTEND/src/assets/pages/login.jsx
@@ -1,17 +1,12 @@
 import { Link } from "react-router-dom";
-import HeaderTopDeals from "../components/headerTopDeals";
-import HeroSlider from "../components/hero2";
 import Nav from "../components/nav2";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import ForgorPass from "./forgotPass";
 import Footer from "../components/footer";
 const Login = () => {
   return (
     <section>
       <section>
-        <HeaderTopDeals />
         <Nav />
-        <HeroSlider />
       </section>
       <section className="flex flex-col items-center bg-primary px-20 py-24">
         <h1 className="text-4xl font-semibold mb-16">Login</h1>
@@ -44,20 +39,20 @@ const Login = () => {
               <button className="bg-black text-primary text-lg py-2 px-4 rounded-md w-[10rem] mt-4">
                 Login
               </button>
-              <div className="relative top-2 left-10 hover:text-primary hover:bg-black border border-slate-950 py-2 px-6 rounded-md w-[12rem] transition-all duration-500">
-                <Link to="/forgot-password">
-                  <p className="text-lg">Forgot Password</p>
+              <div className="relative top-2 left-8 hover:text-primary hover:bg-black border border-slate-950 py-2 px-6 rounded-md w-[13rem] transition-all duration-500">
+                <Link to="/register">
+                  <p className="text-lg">Create an Account</p>
                 </Link>
               </div>
             </div>
             <div className="relative top-6 text-center text-lg">
               <Link
-                to="/register"
-                className="relative left-7 flex items-center"
+                to="/forgot-password"
+                className="flex items-center"
               >
-                <p className="mr-2">Dont have an Account? </p>
+                <p className="mr-2">Dont remember your password? </p>
                 <p className="hover:text-blue-700 hover:underline">
-                  Create an Account
+                  Recover Password
                 </p>
               </Link>
             </div>
