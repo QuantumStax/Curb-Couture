6da38efd46ea35e32a2dbedab9584d780184bd59
QuantumStax <quantumstax@gmail.com>
2025-01-28 23:17:19 +0530

    view card - reviews

--- FRONTEND/src/assets/components/productView.jsx
+++ FRONTEND/src/assets/components/productView.jsx
@@ -4,6 +4,9 @@ import Footer from "./footer";
 import Nav from "./nav2";
 import StraightenIcon from "@mui/icons-material/Straighten";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import UndoIcon from "@mui/icons-material/Undo";
+import VerifiedIcon from "@mui/icons-material/Verified";
 
 const ProductView = () => {
   const [showOffers, setShowOffers] = useState(false);
@@ -21,6 +24,63 @@ const ProductView = () => {
     {
       coupon: "FREESHIP",
       desc: "Free shipping on your first order!",
+    },
+  ];
+  const reviews = [
+    {
+      rating: "⭐⭐⭐⭐⭐",
+      name: "Gopikrishnan S",
+      varified: true,
+      district: "Alappuzha",
+      country: "IN",
+      head: "Great Product",
+      desc: "superb quality at this price point!",
+      date: "01/03/2024",
+      img: "/images/collection/oversized/medusa-oversized-t-shirt-753500.webp",
+    },
+    {
+      rating: "⭐⭐⭐⭐⭐",
+      name: "Gopikrishnan S",
+      varified: true,
+      district: "Alappuzha",
+      country: "IN",
+      head: "Great Product",
+      desc: "superb quality at this price point!",
+      date: "01/03/2024",
+      img: "/images/collection/oversized/medusa-oversized-t-shirt-753500.webp",
+    },
+    {
+      rating: "⭐⭐⭐⭐⭐",
+      name: "Gopikrishnan S",
+      varified: true,
+      district: "Alappuzha",
+      country: "IN",
+      head: "Great Product",
+      desc: "superb quality at this price point!",
+      date: "01/03/2024",
+      img: "/images/collection/oversized/medusa-oversized-t-shirt-753500.webp",
+    },
+    {
+      rating: "⭐⭐⭐⭐⭐",
+      name: "Gopikrishnan S",
+      varified: true,
+      district: "Alappuzha",
+      country: "IN",
+      head: "Great Product",
+      desc: "superb quality at this price point!",
+      date: "01/03/2024",
+      img: "/images/collection/oversized/medusa-oversized-t-shirt-753500.webp",
+    },
+    {
+      rating: "⭐⭐⭐⭐⭐",
+      name: "Gopikrishnan S",
+      varified: true,
+      district: "Alappuzha",
+      country: "IN",
+      head: "Great Product",
+      desc: "superb quality at this price point!",
+      date: "01/03/2024",
+      img: "/images/collection/oversized/medusa-oversized-t-shirt-753500.webp",
     },
   ];
 
@@ -144,7 +204,92 @@ const ProductView = () => {
                   </div>
                 ))}
               </div>
+              {/* general info */}
+              <div className="text-lg">
+                <div className="flex items-center gap-2">
+                  <AccessTimeIcon />
+                  <p>Estimate delivery time: 3 - 5 business day</p>
+                </div>
+                <p>
+                  <span className="font-bold">Note : </span>The orders are
+                  delivered through DTDC and the delivery time may vary!
+                </p>
+                <div className="flex items-center gap-2 mt-3">
+                  <UndoIcon />
+                  <p>
+                    Return within 10 days of purchase. Duties & taxes are
+                    non-refundable!
+                  </p>
+                </div>
+              </div>
             </div>
+          </div>
+        </div>
+        {/* Product Description */}
+        <div className="pt-[7rem] w-[75rem]">
+          <h1 className="text-2xl uppercase font-semibold">Product Info</h1>
+          <hr className="w-[11rem] mt-1 h-[0.1rem] bg-black opacity-70" />
+          <div className="text-lg mt-4">
+            <p>
+              The garments labelled as committed are products that have been
+              produced using sustainable fibres or processes, reducing their
+              environmental impact. Umino’s goal is to support the
+              implementation of practices more committed to the environment.
+            </p>
+            <div className="mt-2">
+              <li>Name: Medusa Oversized T-Shirt</li>
+              <li>Fabric: Pure Cotton</li>
+              <li>Occation: Casual</li>
+              <li>Pattern: Screen Printed</li>
+              <li>Sleeve Length: Short Sleeve</li>
+            </div>
+            <p className="mt-2">
+              Made of cotton, you will love the high-quality fit and feel of
+              this Medusa Oversized tee. A fashionable weekend outfit starts
+              with this white top matched with chinos and your favourite pair of
+              sunglasses to cast an effortlessly cool image.
+            </p>
+          </div>
+        </div>
+        {/* Product Review and Rating */}
+        <div className="mt-10">
+          <h1 className="text-2xl uppercase font-semibold">Product Review</h1>
+          <hr className="w-[13rem] mt-1 h-[0.1rem] bg-black opacity-70" />
+          <div className="flex items-center gap-2 mt-4">
+            <h1 className="text-4xl text-green-600">4.4</h1>
+            <p className="text-xl">⭐</p>
+            <p>from 15 Reviews</p>
+          </div>
+          <div className="mt-5">
+            {reviews.map((review, i) => (
+              <div key={i} className="mb-10">
+                <p>{review.rating}</p>
+                <div className="flex items-center gap-2 mt-2">
+                  <h1>{review.name}</h1>
+                  {review.varified ? (
+                    <VerifiedIcon style={{ color: "blue" }} />
+                  ) : (
+                    ""
+                  )}
+                  <div className="relative left-[50rem] opacity-50">
+                    <p>{review.date}</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-1 opacity-50">
+                  <p>{review.district},</p>
+                  <p>{review.country}</p>
+                </div>
+                <h1 className="font-bold my-2 text-xl">{review.head}</h1>
+                <p className="mb-2">{review.desc}</p>
+                <div>
+                  <img
+                    src={review.img}
+                    alt="img"
+                    className="h-[10rem] rounded-lg"
+                  />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
