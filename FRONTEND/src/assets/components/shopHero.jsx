a4f9808d501617421b8b4fc83f35e9358e060759
Gopisam1126 <gopikrishnan0045@gmail.com>
2025-02-05 19:06:49 +0530

    update theme

--- FRONTEND/src/assets/components/shopHero.jsx
+++ FRONTEND/src/assets/components/shopHero.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const ShopHero = ({ image, heading }) => {
   return (
-    <div className="relative h-[30rem] bg-red-800" id="hero">
+    <div className="relative h-[30rem] bg-tertiary" id="hero">
       <img src={image} alt="hero-img" className="lg:ml-20 pt-10" />
       <div className="absolute top-[50%] left-[50%] lg:translate-x-[0] md:translate-x-0 translate-x-[-50%] translate-y-[-50%] lg:w-[40rem] w-fit p-5 text-primary font-extrabold font-itim">
         <h1 className="w-fit lg:text-9xl text-7xl uppercase">{heading}</h1>
--- FRONTEND/tailwind.config.js
+++ FRONTEND/tailwind.config.js
@@ -6,13 +6,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#FEFAF0",
+        // primary: "#FEFAF0",
+        primary: "#ECF0F1",
         secondary: "#C4C197",
-        tertiary: "#DDDDCC",
-        banner: "#E8E59B",
-        brand: "#FFFCE6",
+        // tertiary: "#DDDDCC",
+        tertiary: "#2C3E50",
+        // banner: "#E8E59B",
+        banner: "#578E7E",
+        // brand: "#FFFCE6",
+        brand: "#48A6A7",
         "text-main": "#403F2B",
-        "cupon-bg": "#FCE2E1",
+        // "cupon-bg": "#FCE2E1",
+        "cupon-bg": "#9ACBD0",
       },
       fontFamily: {
         poppins: ["Poppins", "serif"],
@@ -29,14 +34,28 @@ export default {
 
 // pallet - 1
 
-// primary = "2C3E50"  -> Main text, headers, branding elements
-// secondary = "ECF0F1" -> Backgrounds, sections
-// tertiary = "E74C3C" -> Primary buttons, call-to-action elements
-// accent = "3498DB" -> Links, hover effects, secondary buttons
+// primary = "2C3E50"  -> Main text, headers, branding elements - dark
+// secondary = "ECF0F1" -> Backgrounds, sections - light blue-white
+// tertiary = "E74C3C" -> Primary buttons, call-to-action elements - red light
+// accent = "3498DB" -> Links, hover effects, secondary buttons - sky blue
 
 // pallet - 2
 
-// primary = "1E1E1E"  -> Text, headers, logo, navbar
-// secondary = "F5F5F5" -> Background, product cards, sections
-// tertiary = "FF6B6B" -> Buttons (Buy Now, Add to Cart), CTA elements
-// accent = "007AFF" -> Links, hover effects, secondary buttons
+// primary = "1E1E1E"  -> Text, headers, logo, navbar - darker
+// secondary = "F5F5F5" -> Background, product cards, sections - whitish
+// tertiary = "FF6B6B" -> Buttons (Buy Now, Add to Cart), CTA elements - ligher redish
+// accent = "007AFF" -> Links, hover effects, secondary buttons - darker sky blue
+
+// Alex recomended 
+
+// #F2EFE7 - white with tone
+// #9ACBD0 - green bluish tone
+// #48A6A7 - dark green bluish tone
+// #2973B2 - dark bluish tone
+
+// boss recomended
+
+// #000957
+// #344CB7
+// #577BC1
+// #578E7E
